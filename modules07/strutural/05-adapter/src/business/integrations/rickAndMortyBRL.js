6328fda952a391f037d69deebaca05fe97e5d17b
Andreazo Silva <andreazocoisas@gmail.com>
2021-05-09 13:35:21 -0300

    added adpter pattern

---
+++ modules07/strutural/05-adapter/src/business/integrations/rickAndMortyBRL.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import Character from '../../entities/character.js'
+const URL = ''
+export default class RickAndMortyBRL {
+  static async getCharactersFromJSON() {
+    const { data: { results = [] } } = await axios.get(URL)
+    return results.map(char => new Character(char))
+  }
+}
